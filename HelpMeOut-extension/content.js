f2f5a166934077be855f61c4afe725e4346a76c3
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-10-01 13:59:20 +0100

    extension now working

---
+++ HelpMeOut-extension/content.js
@@ -0,0 +1,55 @@
+console.log("hi, i have been injected");
+var recorder = null;
+function onAccessApproved(stream) {
+  recorder = new MediaRecorder(stream);
+
+  recorder.start();
+  recorder.onstop = function () {
+    stream.getTracks().forEach(function (track) {
+      if (track.readyState === "live") {
+        track.stop();
+      }
+    });
+  };
+  recorder.ondataavailable = function (event) {
+    let recordedBlob = event.data;
+    let url = URL.createObjectURL(recordedBlob);
+    let a = document.createElement("a");
+
+    a.style.display = "none";
+    a.href = url;
+
+    a.download = "scrren-recording.webm";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "request_recording") {
+    console.log("requesting recording");
+
+    sendResponse(`processed: ${message.action}`);
+    navigator.mediaDevices
+      .getDisplayMedia({
+        audio: true,
+        video: {
+          width: 99999999,
+          height: 99999999,
+        },
+      })
+      .then((stream) => {
+        onAccessApproved(stream);
+      });
+  }
+
+  if (message.action === "stopvideo") {
+    console.log("stopping video");
+    sendResponse(`processed: ${message.action}`);
+    if (!recorder) return console.log("no recorder");
+
+    recorder.stop();
+  }
+});
---
+++ HelpMeOut-extension/js/popup.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //GET THE SELECTORS OF THE BUTTON
+  const startVideoButton = document.querySelector("button#start_video");
+  const stopVideoButton = document.querySelector("button#stop_video");
+  //adding event listener
+
+  startVideoButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "request_recording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 16");
+          }
+        }
+      );
+    });
+  });
+
+  stopVideoButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "stopvideo" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 32");
+          }
+        }
+      );
+    });
+  });
+});
